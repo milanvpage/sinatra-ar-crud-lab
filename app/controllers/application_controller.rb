ad148c995f98d7d9d81081c9894eb87432262fc5
Milan <milanvpage@gmail.com>
2021-06-02 11:15:48 -0700

    commit tree 3964c30c1ba3cc6786d7bc1f9c8bbf8310a0da31

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,53 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  get '/articles' do 
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title],
+                    content: params[:content])
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    article = Article.find(params[:id])
+    article.destroy
+
+    redirect '/articles'
+  end
+
+  post '/articles' do 
+    new_article_title = params[:title]
+    new_article_content = params[:content]
+
+    @article = Article.create(title: new_article_title, content: new_article_content)
+
+    redirect "/articles/#{@article.id}"
+  end
+
+
 end
